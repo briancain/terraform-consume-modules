75c9d9a54aedbe127b7577101cef8fac75a4f9ec
Brian Cain <bcain@hashicorp.com>
2020-08-19 16:53:54 -0700

    Add leading 0 to module version

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "null_resource" "hello_world" {
 
 module "helloworld" {
   source  = "briancain/helloworld/aws"
-  version = "2020.4.21"
+  version = "2020.04.21"
   password = "password"
 }
