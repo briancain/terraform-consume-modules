69ebd803697e4b48b0c4113162a469d4a52ccd79
Brian Cain <bcain@hashicorp.com>
2020-08-19 16:34:34 -0700

    Use test module from public registry

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "null_resource" "hello_world" {
   }
 }
 
-module "hello_world" {
-  environment = var.environment
-  source      = "https://tfe-zone-1533d000.ngrok.io/app/hashicorp/modules/view/helloworld/aws"
-  version     = "2020.04.21"
+module "helloworld" {
+  source  = "briancain/helloworld/aws"
+  version = "2020.4.21"
+  password = "password"
 }
